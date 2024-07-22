2c6d72131f8cd15d560112c640992c6321f6d8ff
vinicio <viniciodossantosoliveira@hotmail.com>
2024-07-22 18:47:18 -0300

    ultimo commit

--- test/src/json_placeholdeer/repositories/to_do_repository_test.dart
+++ test/src/json_placeholdeer/repositories/to_do_repository_test.dart
@@ -1,5 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('ToDoRepository', () {});
+  group('ToDoRepository.getToDos', () {
+    test('deve fazer ', () {
+
+    });
+  });
 }
